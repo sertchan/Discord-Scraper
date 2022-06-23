7b86aa52a025ec1526227af979131c12f4863eee
sertchan <ardaseyhantemli1@gmail.com>
2022-06-23 16:11:01 +0300

    done

---
+++ src/index.js
@@ -0,0 +1,106 @@
+import 'dotenv/config'
+
+import { Client } from "discord.js-selfbot-v13"
+import Database from "easy-json-database"
+import chalkAnimation from 'chalk-animation'
+import { createSpinner } from "nanospinner"
+import downapi from 'follow-redirects'
+import figlet from "figlet"
+import fs from 'fs'
+import fsExtra from 'fs-extra'
+import gradient from "gradient-string"
+import uniqid from 'uniqid'
+
+const { https } = downapi
+
+
+const client = new Client()
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
+
+function download(url) {
+    return new Promise((resolve, reject) => {
+        https.get(url, (res) => {
+            res.pipe(fs.createWriteStream(`src\\pfp\\${process.env.GUILDID}\\${uniqid()}.${process.env.FORMAT}`))
+            .on('error', reject)
+            .once('close', () => resolve(`src\\pfp\\${process.env.GUILDID}\\${uniqid()}.${process.env.FORMAT}`))
+        })
+    })
+}
+
+async function shuffle(sourceArray) {
+    for (var i = 0; i < sourceArray.length - 1; i++) {
+        var j = i + Math.floor(Math.random() * (sourceArray.length - i))
+        var temp = sourceArray[j]
+        sourceArray[j] = sourceArray[i]
+        sourceArray[i] = temp
+    }
+    return sourceArray
+}
+
+async function files() {
+    if (!fs.existsSync(`src\\database\\${process.env.GUILDID}`)) {
+    fs.mkdirSync(`src\\database\\${process.env.GUILDID}`)
+    }
+
+    if (!fs.existsSync(`src\\username\\${process.env.GUILDID}`)) {
+        fs.mkdirSync(`src\\username\\${process.env.GUILDID}`)
+    }
+
+    if (!fs.existsSync(`src\\pfp\\${process.env.GUILDID}`)) {
+        fs.mkdirSync(`src\\pfp\\${process.env.GUILDID}`)
+    }
+
+    if (fs.existsSync(`src\\pfp\\${process.env.GUILDID}`)) {
+        fsExtra.emptyDirSync(`src\\pfp\\${process.env.GUILDID}`)
+    }
+
+    await fs.createWriteStream(`src\\username\\${process.env.GUILDID}\\username.txt`, { overwrite: false })
+    fs.truncateSync(`src\\username\\${process.env.GUILDID}\\username.txt`, 0)
+}
+
+
+async function welcome() {
+    var author = chalkAnimation.karaoke(`[Ready] Loggined as ${client.user.tag}\n`)
+    figlet(`Discord Server Scraper`, async(_err, data) => {
+        console.log(gradient.pastel.multiline(data))
+        await author.start()
+    })
+    await sleep(2500)
+    author.stop()
+}
+
+async function scrape() {
+    const db = new Database(`./src/database/${process.env.GUILDID}/database.json`)
+    var memberid =  await shuffle((await (await client.guilds.fetch(process.env.GUILDID)).members.fetch()).filter(x => x.user.id !== null && !x.user.bot).map(r => r.user.id))
+    db.set('id', memberid )
+    db.set('url', [])
+    let l = memberid.length
+    for (let i = 0; i < l; i++) {
+        let spinner = createSpinner("Scraping...").start()
+        let member = await client.users.fetch(memberid[i])
+        let url = await member.displayAvatarURL({ forceStatic: true }).replace(".webp", `.${process.env.FORMAT}`)
+        fs.appendFileSync(`src\\username\\${process.env.GUILDID}\\username.txt`, member.username + '\r\n')
+        if (!url.includes('embed')) {
+            db.push(`url`, url)         
+            try {
+                await download(url + '?size=4096')
+            }
+            catch(err) {
+                console.log(err)
+            }
+            spinner.success({ text: ` %${((100* i) / memberid.length).toFixed(3) } >> ${(member.username).replace(/[\u0250-\ue007]/g, '')}` })
+        }
+    }
+    console.log('finished.')
+    process.exit(0)
+}
+
+client.on('ready', async() => {
+    await files()
+    await welcome()
+    scrape()
+})
+
+
+client.login(process.env.TOKEN)
