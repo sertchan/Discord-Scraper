3771bbe3d9cf67028b27ee28b42d37dffbbbf20f
Seyhan <ardaseyhantemli1@gmail.com>
2022-08-03 01:58:06 +0300

    Simplified & Optimized ]things.

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import fs from 'fs'
 import fsExtra from 'fs-extra'
 import gradient from "gradient-string"
 import https from 'https'
-import uniqid from 'uniqid'
 
 const client = new Client({
         checkUpdate: false,
@@ -17,12 +16,12 @@ const client = new Client({
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
 
-function download(url) {
+function download(url,hash,guild) {
     return new Promise((resolve, reject) => {
         https.get(url, (res) => {
-            res.pipe(fs.createWriteStream(`src\\pfp\\${process.env.GUILDID}\\${uniqid()}.${process.env.FORMAT}`))
+            res.pipe(fs.createWriteStream(`src\\${guild}\\pfp\\${hash}.${process.env.FORMAT}`))
             .on('error', reject)
-            .once('close', () => resolve(`src\\pfp\\${process.env.GUILDID}\\${uniqid()}.${process.env.FORMAT}`))
+            .once('close', () => resolve(`src\\${guild}\\pfp\\${hash}.${process.env.FORMAT}`))
         })
     })
 }
@@ -37,80 +36,64 @@ async function shuffle(sourceArray) {
     return sourceArray
 }
 
-async function files() {
-    if (!fs.existsSync(`src\\database`)) {
-        await fs.mkdirSync(`src\\database`)
+async function files(guild) {
+    if (!fs.existsSync(`src\\${guild}`)) {
+        await fs.mkdirSync(`src\\${guild}`)
     }
 
-    if (!fs.existsSync(`src\\database\\${process.env.GUILDID}`)) {
-     fs.mkdirSync(`src\\database\\${process.env.GUILDID}`)
+    if (!fs.existsSync(`src\\${guild}\\pfp`)) {
+        fs.mkdirSync(`src\\${guild}\\pfp`)
     }
 
-    if (!fs.existsSync(`src\\username`)) {
-        await fs.mkdirSync(`src\\username`)
+    if (fs.existsSync(`src\\${guild}\\pfp`)) {
+        fsExtra.emptyDirSync(`src\\${guild}\\pfp`)
     }
-
-    if (!fs.existsSync(`src\\username\\${process.env.GUILDID}`)) {
-       await fs.mkdirSync(`src\\username\\${process.env.GUILDID}`)
-    }
-
-    if (!fs.existsSync(`src\\pfp`)) {
-       await fs.mkdirSync(`src\\pfp`)
-    }
-
-    if (!fs.existsSync(`src\\pfp\\${process.env.GUILDID}`)) {
-        fs.mkdirSync(`src\\pfp\\${process.env.GUILDID}`)
-    }
-
-    if (fs.existsSync(`src\\pfp\\${process.env.GUILDID}`)) {
-        fsExtra.emptyDirSync(`src\\pfp\\${process.env.GUILDID}`)
-    }
-    await fs.createWriteStream(`src\\username\\${process.env.GUILDID}\\username.txt`, { overwrite: false })
+    await fs.createWriteStream(`src\\${guild}\\username.txt`, { overwrite: false })
     await sleep(500)
-    fs.truncateSync(`src\\username\\${process.env.GUILDID}\\username.txt`, 0)
+    fs.truncateSync(`src\\${guild}\\username.txt`, 0)
 }
 
 
-async function welcome() {
+async function welcome(guild) {
     let author = chalkAnimation.karaoke(`[Ready] Loggined as ${client.user.tag}\n`)
-    figlet(`Discord Server Scraper`, async(_err, data) => {
+    figlet(`${guild}`, async(_err, data) => {
         console.log(gradient.pastel.multiline(data))
         await author.start()
     })
-    await sleep(2500)
     author.stop()
 }
 
-async function scrape() {
-    const db = new Database(`./src/database/${process.env.GUILDID}/database.json`)
-    let memberid =  await shuffle((await (await client.guilds.fetch(process.env.GUILDID)).members.fetch({limit: 0})).filter(x => x.user.id !== null && !x.user.bot).map(r => r.user.id))
+async function scrape(guild) {
+    const db = new Database(`./src/${guild}/database.json`);
+    let memberid =  await shuffle((await (await client.guilds.fetch(guild)).members.fetch({limit: 0})).filter(x => x.user.id !== null && !x.user.bot).map(r => r.user.id))
     db.set('id', memberid )
-    db.set('url', [])
+    db.set('pfp_url', [])
     let l = memberid.length
-    for (let i = 0; i < l; i++) {
-        let spinner = createSpinner("Scraping...").start()
+    for (var i = 0; i < l; i++) {
         let member = await client.users.fetch(memberid[i])
-        let url = await member.displayAvatarURL({ forceStatic: true }).replace(".webp", `.${process.env.FORMAT}`)
-        fs.appendFileSync(`src\\username\\${process.env.GUILDID}\\username.txt`, member.username + '\r\n')
-        if (!url.includes('embed')) {
-            db.push(`url`, url)         
-            try {
-                await download(url + '?size=4096')
-            }
-            catch(err) {
-                console.log(err)
-            }
-            spinner.success({ text: ` %${((100* i) / memberid.length).toFixed(3) } >> ${(member.username).replace(/[\u0250-\ue007]/g, '')}` })
-        }
+        fs.appendFileSync(`src\\${guild}\\username.txt`, member.username + '\r\n')
+        if (member.avatar) {
+            let spinner = createSpinner().start()
+                let pfp_url = await member.displayAvatarURL({ forceStatic: true }).replace(".webp", `.${process.env.FORMAT}`)
+                db.push(`pfp_url`, pfp_url)         
+                try {
+                    await download(pfp_url + '?size=4096', member.avatar,guild)
+                }
+                catch(err) {
+                    console.log(err)
+                }
+            spinner.success({ text: `[${((100* i) / memberid.length).toFixed(3)}%] User: ${(member.username).replace(/[\u0250-\ue007]/g, '')}#${member.discriminator}, ID: ${member.id}`})
     }
-    console.log('finished.')
-    process.exit(0)
+}
+console.log(`${i} user succesfully scraped from ${guild} (${process.env.GUILDID}). Have a good day!`)
+process.exit(0)
 }
 
 client.on('ready', async() => {
-    await files()
-    await welcome()
-    scrape()
+    let guild = await client.guilds.fetch(process.env.GUILDID)
+    await files(guild)
+    await welcome(guild)
+    scrape(guild)
 })
 
 
